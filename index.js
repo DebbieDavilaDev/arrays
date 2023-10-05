e1207e7ab70eeb88cf87bf95841a86fc3b99306f
Debbie Davila <macbookair@Debbie-Davilas-MacBook-Air.local>
2023-10-05 15:48:26 -0400

    first commit

---
+++ index.js
@@ -0,0 +1,17 @@
+const nums = [3, 5, 7, 2]//This is an array on numbers.
+const nums2 = [3,4, 6, 4]//This is also an array of numbers.
+
+// console.log(nums[2]) // This will output the number 7.
+
+let fruits = ['banana', 'apple', 'kiwi']//This is an array of stings.
+
+console.log(fruits[1])// This will output apple.
+
+fruits[0] = "pineapple"// Change the value of the first item to 'pineapple'
+
+console.log(fruits[0])//This will output pineapple
+
+let things = [6, "Todd", false, "false", [2]] //This array has different data types in it, including another array!
+
+console.log(things[4])
+
